b16e9fc6dcb3063379433af7ed740219003027e5
sebmendo1 <contact@sebastianmendo.design>
2025-09-30 21:53:10 -0500

    Initial Commit

---
+++ MeetMemento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetMemento
+//
+//  Created by Sebastian Mendo on 9/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetMemento/MeetMementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMementoApp.swift
+//  MeetMemento
+//
+//  Created by Sebastian Mendo on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetMementoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
